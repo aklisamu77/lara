3f6225de6f859722b5aa71efc20c37e85441d06f
aklisamu77 <aklisamu77@gmail.com>
2021-05-11 15:12:02 +0200

    route home 2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Lara</title>
+            <style>
+                body {
+    background: #efefef;
+}
+h3{
+    
+    text-align: center;
+    font-size: 80px;
+    padding: 120px 0 0 0;
+    color: #333;
+    font-family: monospace;
+
+}
+ul{
+    list-style: none;
+    text-align: center;
+}
+ul li {
+    display: inline-block;
+    margin: 0 20px;
+}
+            </style>
+    </head>
+    <body>
+        <h3>Lara Home</h3>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="/about-us">About Us</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </body>
+</html>
